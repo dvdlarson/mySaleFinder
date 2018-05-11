ed83847a9d2bcc96264900a1309e57689f20bae0
cdplourde <cdplourde@gmail.com>
2018-05-11 14:18:20 -0700

    basic server and homepage route

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var path = require("path");
+
+var app = express();
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
+app.set("port", (process.env.PORT || 8080));
+
+app.use("/public", express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/mysales.js");
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(app.get("port"), function() {
+    console.log("Server started on port " + app.get("port"));
+});
