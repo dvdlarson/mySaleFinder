f7ad9e8080e759eb8fddaad0ec88ea86b2c5e0dd
cdplourde <cdplourde@gmail.com>
2018-05-15 20:16:39 -0700

    I forgot the button..

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-    $("#username").inputmask("99-9999999");  //static mask
-    $("#username").inputmask({"mask": "(999) 999-9999"}); //specifying options
-    $("#username").inputmask("9-a{1,3}9{1,3}"); //mask with dynamic syntax
+    $("#phone").inputmask("999-999-9999");  //static mask
+    $("#phone").inputmask({"mask": "(999) 999-9999"}); //specifying options
   });
