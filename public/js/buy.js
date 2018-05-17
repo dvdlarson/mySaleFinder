810a483e140042f1bedbb4926d5a6ddb30d42e01
cdplourde <cdplourde@gmail.com>
2018-05-17 04:08:19 -0700

    user position completed.. map now defaults to users location

--- public/js/buy.js
+++ public/js/buy.js
@@ -1,7 +1,27 @@
 var map;
+function getPos() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        map.setCenter(initialLocation);
+    });
+  }    
+}
+
 function initMap() {
+  //display map
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {
+      lat: 33,
+      lng: 112
+    },
+    zoom: 11
   });
 }
+
+$(document).ready(function () {
+  //Create new map and go to users position
+  initMap();
+  getPos();
+});
+
