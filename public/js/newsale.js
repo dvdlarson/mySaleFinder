2babd401de56b643eab747e6dfc50a6c8e4490a1
Briana Oursler <boursler@blackhole.localdomain>
2018-05-19 19:27:12 -0700

    Rearrange refs in html and remove references to datepicker, replace with .calendar

--- public/js/newsale.js
+++ public/js/newsale.js
@@ -1,30 +1,33 @@
-
-
 $(document).ready(function () {
-    $('#example1').calendar();
+    // $('#example1').calendar();
 
-    $("#rangestart").timepicker();
-     
+    $('#rangestart').calendar({
+        type: 'date',
+        endCalendar: $('#rangeend')
+    });
 
-      $('#rangeend').calendar({
+
+    $('#rangeend').calendar({
         type: 'date',
         startCalendar: $('#rangestart')
-      });
-    
-      $('#startTime').timepicker();
-      $('#endTime').calendar({
+    });
+
+    $('#startTime').calendar({
         type: 'time'
-      });
-      $('.ui.radio.checkbox')
-      .checkbox();
-      $('select.dropdown')
-      .dropdown();
-console.log("loaded the functions");
+    });
+    $('#endTime').calendar({
+        type: 'time'
+    });
+    $('.ui.radio.checkbox')
+        .checkbox();
+    $('select.dropdown')
+        .dropdown();
+    console.log("loaded the functions");
     //add a burger
     $(".addsale").on("click", function (event) {
         event.preventDefault();
-        var fullAddress=$("#address").val().trim()+" "+$("#city").val().trim()+" "+$("#state").val().trim()+" "+$("#zip").val().trim()
-        console.log("full addr: "+fullAddress);
+        var fullAddress = $("#address").val().trim() + " " + $("#city").val().trim() + " " + $("#state").val().trim() + " " + $("#zip").val().trim()
+        console.log("full addr: " + fullAddress);
 
 
         var newSale = {
@@ -33,17 +36,17 @@ console.log("loaded the functions");
             start_date: $("#rangestart").val().trim(),
             end_date: $("#rangeend").val().trim(),
             start_time: $("#startTime").val().trim(),
-            end_time:$("#endTime").val().trim(),
-            on_street_parking:$("#parking").val().trim(),
-            inside_outside:$("#inside_outside").val().trim(),
-            weather_cancel:$("#weather_cancel").val().trim(),
-            items_desc:$("#desc").val().trim(),
+            end_time: $("#endTime").val().trim(),
+            on_street_parking: $("#parking").val().trim(),
+            inside_outside: $("#inside_outside").val().trim(),
+            weather_cancel: $("#weather_cancel").val().trim(),
+            items_desc: $("#desc").val().trim(),
             city: $("#city").val().trim(),
             state: $("#state").val().trim(),
             zip_cd: $("#zip").val().trim(),
-            full_address:fullAddress,
-            active:1,
-            UserId:req.session.user.id
+            full_address: fullAddress,
+            active: 1,
+            UserId: req.session.user.id
 
         };
         console.log(newSale);
@@ -56,8 +59,5 @@ console.log("loaded the functions");
         });
     });
 
-    
+
 });
-
-
-
