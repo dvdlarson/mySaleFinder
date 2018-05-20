9e8abb647b991f8da112898018f34fa9a0911175
cdplourde <cdplourde@gmail.com>
2018-05-20 12:42:54 -0700

    logo font

--- public/js/buy.js
+++ public/js/buy.js
@@ -15,43 +15,50 @@ function getPos() {
           },
           zoom: 11
         });
-        $.get("/api/buy", function(data) {
-          //loop through sales
-          for (var i = 0; i < data.length; i++) {
-            //display the markers for every sale
-            var marker = new google.maps.Marker({
-              position: {
-                lat: data[i].latitude,
-                lng: data[i].longitude
-              },
-              map: map,
-              title: data[i].title, 
-              id: (i + 1)
-            });
-            //marker info window content
-            marker.content = "<h3>" + data[i].title + "</h3>";
-            //display info window on marker click and scroll to the relevant sale
-            var infoWindow = new google.maps.InfoWindow();
-            google.maps.event.addListener(marker, 'click', function () {
-                infoWindow.setContent(this.content);
-                infoWindow.open(this.getMap(), this);
-                //scroll to sale
-                $("#cards-wrapper").animate({scrollTop: $("#cards-wrapper").scrollTop() + $("#" + this.id).position().top});
-                //reset border highlights
-                $("li").css({"border-left": "none"});
-                $(".card").attr("style", "margin-left: 6px!important");
-                //selected sale border highlight
-                $("#" + this.id).css({"border-left": "6px solid gold"});
-                $("#" + this.id + " .card").attr("style", "margin-left: 0px!important");
-            }); 
-            //close window if you click anywhere else on the map
-            map.addListener("click", function(event) {
-              infoWindow.close();
-            });
-          }
-        });
+        drawMarkers();
     });
   }
+}
+
+function drawMarkers() {
+  $.get("/api/buy", function(data) {
+    //loop through sales
+    for (var i = 0; i < data.length; i++) {
+      //display the markers for every sale
+      var marker = new google.maps.Marker({
+        position: {
+          lat: data[i].latitude,
+          lng: data[i].longitude
+        },
+        map: map,
+        title: data[i].title, 
+        id: (i + 1)
+      });
+      //marker info window content
+      marker.content = "<h3>" + data[i].title + "</h3>";
+      //display info window on marker click and scroll to the relevant sale
+      var infoWindow = new google.maps.InfoWindow();
+      google.maps.event.addListener(marker, 'click', function () {
+          infoWindow.setContent(this.content);
+          infoWindow.open(this.getMap(), this);
+          //scroll to sale
+          $("#cards-wrapper").stop( true );
+          $("#cards-wrapper").animate({scrollTop: $("#cards-wrapper").scrollTop() + $("#" + this.id).position().top});
+          //reset border highlights
+          $("li").css({"border-left": "none"});
+          $(".card").attr("style", "margin-left: 6px!important");
+          //selected sale border highlight
+          $("#" + this.id).css({"border-left": "6px solid gold"});
+          $("#" + this.id + " .card").attr("style", "margin-left: 0px!important");
+      }); 
+      //close window if you click anywhere else on the map
+      map.addListener("click", function(event) {
+        infoWindow.close();
+        $("li").css({"border-left": "none"});
+        $(".card").attr("style", "margin-left: 6px!important");
+      });
+    }
+  });
 }
 
 $(document).ready(function () {
